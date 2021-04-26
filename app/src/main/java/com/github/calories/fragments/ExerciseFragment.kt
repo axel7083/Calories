70ac727710d519303d5a11c70fbef756a08a2480
axel7083 <42176370+axel0070@users.noreply.github.com>
2021-04-26 13:31:05 +0200

    Adding SimpleCountDownTimer
    This class was made by Mobin Munir. I edited it for my needs.

--- app/src/main/java/com/github/calories/fragments/ExerciseFragment.kt
+++ app/src/main/java/com/github/calories/fragments/ExerciseFragment.kt
@@ -1,39 +1,26 @@
 package com.github.calories.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.github.calories.DatabaseHelper
 import com.github.calories.databinding.FragmentExerciseBinding
-import com.github.calories.databinding.FragmentStatsBinding
 import com.github.calories.models.Exercise
-import com.github.calories.models.RawValues
-import com.github.calories.models.Stats
-import com.github.calories.utils.CustomBarChartRender
-import com.github.calories.utils.ThreadUtils
+import com.github.calories.utils.SimpleCountDownTimer
 import com.github.calories.utils.UtilsTime.*
-import com.github.mikephil.charting.components.XAxis
-import com.github.mikephil.charting.data.BarData
-import com.github.mikephil.charting.data.BarDataSet
-import com.github.mikephil.charting.data.BarEntry
-import com.github.mikephil.charting.formatter.ValueFormatter
-import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
-import java.time.LocalDate
-import java.time.temporal.TemporalField
-import java.time.temporal.WeekFields
 import java.util.*
-import kotlin.collections.ArrayList
 
 
-class ExerciseFragment : Fragment() {
+class ExerciseFragment : Fragment(), SimpleCountDownTimer.OnCountDownListener {
 
     private lateinit var binding: FragmentExerciseBinding
     private lateinit var db: DatabaseHelper
     private var exerciseEvent: ExerciseEvent? = null
     private lateinit var exercise: Exercise
+
+    private var simpleCountDownTimer: SimpleCountDownTimer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,6 +40,21 @@ class ExerciseFragment : Fragment() {
 
         if(exercise.time != null) {
             binding.timeLeft.text = exercise.time.toString()
+
+            binding.btnPause.setOnClickListener {
+                if(simpleCountDownTimer!!.isPaused) {
+                    binding.btnPause.text = "Pause"
+                    simpleCountDownTimer!!.resume()
+                }
+                else {
+                    binding.btnPause.text = "Resume"
+                    simpleCountDownTimer!!.pause()
+                }
+            }
+
+
+            simpleCountDownTimer = SimpleCountDownTimer(0,exercise.time!!.toLong(),1,this)
+            simpleCountDownTimer!!.start()
         }
         else
         {
@@ -65,8 +67,8 @@ class ExerciseFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View {
 
         // Inflate the layout for this fragment
@@ -98,5 +100,13 @@ class ExerciseFragment : Fragment() {
 
     companion object {
         private const val TAG: String = "StatsFragment"
+    }
+
+    override fun onCountDownActive(time: String?) {
+        binding.timeLeft.text = time
+    }
+
+    override fun onCountDownFinished() {
+        exerciseEvent?.onFinish()
     }
 }
